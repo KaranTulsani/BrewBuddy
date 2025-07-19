ca60a425bf6f34df1665e8fe4290cbf5ba6831b6
Karan Tulsani <your.email@example.com>
2025-07-19 20:11:59 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // 🟨 Add this line to fix 404 on Vercel
+});
